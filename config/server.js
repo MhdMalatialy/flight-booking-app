a4f331e7ab76e9e0db0d08a160279f0b67ecfbca
Mohammad Malatialy <mohammad.maltialy@gmail.com>
2021-06-25 12:14:01 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '08c85769f2248e487d78c770ad60da0b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2ab6ae9-e664-42d2-9f00-dd0bc0dfdd2c'
+};
